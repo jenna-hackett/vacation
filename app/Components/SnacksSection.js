ab18965933314d05a97ef480bb1835a90ba23387
Jenna Hackett <jenna.hackett96@gmail.com>
2026-01-16 14:05:31 -0700

    - Completed the welcome component. - Added in Verity's snack component and Kate's ride component. - page.js file is in working order (calls on the components to display). - Images added to welcome component. Located inside the public folder. - Some tailwind css generated for photo styling as the images previously took up the entire page.
    - Added in the week-2 folder to the app folder for a little self reminder on how to do it again for assignment 2.

---
+++ app/Components/SnacksSection.js
@@ -0,0 +1,35 @@
+/* Verity's Component */
+
+export default function Snacks() {
+  return (
+    <section>
+      <header>
+        <h2 className="font-extrabold text-xl m-4">Best Snacks</h2>
+      </header>
+      <div>
+        <header>
+          <h3 className="font-bold m-4">Walt Disney World</h3>
+        </header>
+        <ul class="list-disc pl-5">
+          <li>Mickey Premium Ice Cream Bar — A classic fan favorite</li>
+          <li>Dole Whip — Refreshing pineapple soft-serve</li>
+          <li>Churros — Crispy with cinnamon sugar</li>
+          <li>School Bread (Norway Pavilion) — Sweet and delicious</li>
+          <li>Turkey Leg — Hearty and savory snack</li>
+        </ul>
+      </div>
+      <div>
+        <header>
+          <h3 className=" font-bold m-4">Universal Studios Florida</h3>
+        </header>
+        <ul className="list-disc pl-5">
+          <li>Butterbeer — Sweet and creamy wizarding favorite</li>
+          <li>Gobbler Turkey Leg — Giant, flavorful snack</li>
+          <li>Jurassic Park Burger — Tasty and filling</li>
+          <li>Hogsmeade Frozen Butterbeer — Cool version of the classic</li>
+          <li>Universal’s Classic Funnel Cake — Sweet and nostalgic</li>
+        </ul>
+      </div>
+    </section>
+  );
+}
